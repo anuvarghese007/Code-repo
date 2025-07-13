ef7609fcbfe8258efb279e56bac4071898865b41
Sivaprasad <sivaprasadomanakuttan@gmail.com>
2025-07-13 17:35:48 -0400

    added class Circle to utils

---
+++ test_class_circle.py
@@ -0,0 +1,6 @@
+from utils import Circle
+# Example usage
+radius=2
+c = Circle(radius)
+print(c.area())
+print(c.perimeter())
--- utils.py
+++ utils.py
@@ -7,3 +7,31 @@ def palindrome_func(s):
     else:
         return False
 
+
+import math
+
+class Circle:
+    """
+    Gives the features of a circle
+    """
+
+    def __init__(self, radius: float):
+        """
+        Initialize a circle.
+        
+        Args:
+            radius (float): Radius of the circle.
+        """
+        self.radius = radius
+
+    def area(self) -> float:
+        """
+        get the area of the circle
+        """
+        return math.pi * self.radius ** 2
+
+    def perimeter(self) -> float:
+        """
+        get the perimeter of the circle
+        """
+        return 2 * math.pi * self.radius
